ab611bb786403fbeeb7aed1d8f1b4a2292c02214
t22anya <122358743+t22anya@users.noreply.github.com>
2024-11-17 17:42:32 +0530

    Movies clone-1

--- script.js
+++ script.js
@@ -1,14 +1,27 @@
-let container3=document.getElementById('container3');
-let container2=document.getElementById('container2');
-let container1=document.getElementById('container1');
-container1.addEventListener('click',(e)=>{
-    alert('container1')
-})
-container2.addEventListener('click',(e)=>{
-    alert('container2')
-})
+let inpBox = document.getElementById('inp');
+let subButton = document.getElementById('btn');
+let moviesdet = document.getElementById('movies');
 
-container3.addEventListener('click',(e)=>{
-    alert('container3')
-    // e.stopPropagation(); for stop the event listner bubbling behvaiour .
+subButton.addEventListener('click', () => {
+    let inpValue = inpBox.value;
+    fetch(`https://imdb.iamidiotareyoutoo.com/justwatch?q=='${inpValue}'`)
+    .then((res) => {
+        return res.json();
+    }).then((data) => {
+        let movies = data;
+        movies.description.map((movie)=>{
+            let imgURl = movie.photo_url;
+            console.log(imgURl);
+            let card = document.createElement('div');
+            let img = document.createElement('img');
+            let title = document.createElement('h2');
+            img.classList.add('img');
+            card.classList.add('card');
+            img.src = imgURl[0];
+            title.innerText = movie.title;
+            card.appendChild(img);
+            card.appendChild(title);
+            moviesdet.appendChild(card);
+        })
+    })
 })
